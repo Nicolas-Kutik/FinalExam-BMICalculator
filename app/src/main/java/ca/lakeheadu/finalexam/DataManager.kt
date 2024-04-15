9dd1787b349e23e7ccffe80b9a3f02382f3acd32
Nicolas-Kutik <nicolas.kutik@gmail.com>
2024-04-15 11:08:09 -0400

    Updated the DataManager to handle Firebase

--- app/src/main/java/ca/lakeheadu/finalexam/DataManager.kt
+++ app/src/main/java/ca/lakeheadu/finalexam/DataManager.kt
@@ -1,12 +1,75 @@
 package ca.lakeheadu.finalexam
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+
 /**
  * DataManager Singleton
  */
 class DataManager private constructor()
 {
-    companion object
-    {
+    private val db = FirebaseFirestore.getInstance()
+    private val collectionRef = db.collection("BMI")
+
+    fun getBMI(onComplete: (List<Data>) -> Unit) {
+        collectionRef.get()
+            .addOnSuccessListener { result ->
+                val notes = result.mapNotNull { it.toObject<Data>() }
+                onComplete(notes)
+            }
+            .addOnFailureListener {
+                onComplete(emptyList())
+            }
+    }
+
+    fun addBMI(newBMI: Data, onComplete: (Boolean) -> Unit) {
+        collectionRef
+            .add(newBMI)
+            .addOnSuccessListener {
+                onComplete(true)
+            }
+            .addOnFailureListener {
+                onComplete(false)
+            }
+    }
+
+    fun deleteBMIById(BMI_Id: String, onComplete: (Boolean) -> Unit) {
+        collectionRef
+            .document(BMI_Id)
+            .delete()
+            .addOnSuccessListener {
+                onComplete(true)
+            }
+            .addOnFailureListener {
+                onComplete(false)
+            }
+    }
+
+    fun getBMIById(noteId: String, onComplete: (Data?) -> Unit) {
+        collectionRef
+            .document(noteId)
+            .get()
+            .addOnSuccessListener {
+                onComplete(it.toObject<Data>())
+            }
+            .addOnFailureListener {
+                onComplete(null)
+            }
+    }
+
+    fun updateBMI(noteId: String, updatedNote: Data, onComplete: (Boolean) -> Unit) {
+        collectionRef
+            .document(noteId)
+            .set(updatedNote)
+            .addOnSuccessListener {
+                onComplete(true)
+            }
+            .addOnFailureListener {
+                onComplete(false)
+            }
+    }
+
+    companion object {
         val instance: DataManager by lazy { DataManager() }
     }
 }
